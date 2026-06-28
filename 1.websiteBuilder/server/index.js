5b6bbe0ef00fd786b2dc2b212a85fd43f8d10dd9
Rahul Singh <rahulsinghcode07@gmail.com>
2026-06-28 13:01:37 +0530

    Update index.js

--- 1.websiteBuilder/server/index.js
+++ 1.websiteBuilder/server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://one-websitebuilder-1-u4o3.onrender.com",
     credentials:true
 }))
 
