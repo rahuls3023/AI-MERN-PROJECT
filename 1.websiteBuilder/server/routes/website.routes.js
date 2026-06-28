e3f16784163ab511842dd61a112ace36da18d350
Rahul Singh <rahulsinghcode07@gmail.com>
2026-06-28 13:32:50 +0530

    Update website.routes.js

--- 1.websiteBuilder/server/routes/website.routes.js
+++ 1.websiteBuilder/server/routes/website.routes.js
@@ -15,6 +15,6 @@ websiteRouter.get('/get-all',isAuth,getAll)
 
 websiteRouter.get('/deploy/:id',isAuth,deploy)
 
-websiteRouter.get('/get-by-slug/:slug',isAuth,getBySlug)
+websiteRouter.get('/get-by-slug/:slug',getBySlug)
 
 export default websiteRouter 
